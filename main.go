977737f3ff49f666427667be345952de25be71c5
botboy223 <smilekar800@gmail.com>
2024-12-04 19:10:40 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,1 @@
-package main
 
-import (
-	"github.com/gin-gonic/gin"
-)
-
-func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello, world!",
-		})
-	})
-	r.Run(":8080") // Listen and serve on localhost:8080
-}
